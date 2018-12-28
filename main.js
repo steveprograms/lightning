767b9f7a3c21ca341e6b5c7032789b47d3d7d71e
Steven Marks <steveprograms@gmail.com>
2018-12-28 15:53:52 -0600

    adds sample file system, yarn, and react, and webserver

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, electron } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 1600, height: 1000 })
 
   // and load the index.html of the app.
   win.loadFile('index.html')
