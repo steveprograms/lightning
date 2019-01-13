a90830883b0d54819862b88009a43208b9363e7a
Steven Marks <steveprograms@gmail.com>
2019-01-13 13:57:50 -0600

    adds more files

--- app/testMe.js
+++
@@ -1,6 +0,0 @@
-class TestMe {
-  firstFunction () {
-    return 2;
-  }
-}
-module.exports = TestMe;
---
+++ assets/data/defaultStats.js
@@ -0,0 +1,22 @@
+var defaults = {
+  "character":
+  {
+    "stats":
+    {
+      "name":"",
+      "level":1,
+      "xp":0,
+      "credits":0,
+      "inventory": [],
+      "spacecraft":
+      {
+        "model":"Powerman5000",
+        "year":2420,
+        "value":5000,
+        "cargo_space":250,
+      },
+    },
+  },
+};
+
+module.exports = defaults;
