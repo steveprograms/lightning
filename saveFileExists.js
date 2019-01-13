6367f4da2f18490e6f81c7969d22a1c209e7f0f6
chanbot5000 <chandlersterling1@gmail.com>
2019-01-12 20:08:18 -0800

    Updated gamesave before branching
    Now the app checks for the json save file. If it doesn't exist, it copies from the template. The buttons are responsive and "resume game" only displays when a copy of the template exists in the directory. Obligatory lol. lol.

--- saveFileExists.js
+++ saveFileExists.js
@@ -4,7 +4,7 @@ let fs = require('fs')
 var StartNewGame_button = $('<input type="button" value="Start New Game" id="new_game_button"/>')
 var ResumeGame_button = $('<input type="button" value="Resume Existing Game" id="existing_game_button"/>')
 
-if(fs.existsSync('lightning_save')){
+if(fs.existsSync('gamesave1.json')){
   //Return buttons for Start new game and load saved game
   $("#start_game_buttons").append(StartNewGame_button)
   $("#start_game_buttons").append(ResumeGame_button)
@@ -32,7 +32,12 @@ if(fs.existsSync('lightning_save')){
 
 // make a separate function for loading next window?
 function new_game(){
-  alert('new game clicked!')
+  var content = 'this is test shit';
+  fs.copyFile('gamesave.json', 'gamesave1.json', (err) => {
+    if (err) throw err;
+    console.log('new game file was copied to destination.txt');
+  });
+  //alert('new game clicked!')
   //create new save file and load next window
 
 }
