2067409133fe6963741c4d7c82c9445043e71f32
Steven Marks <steveprograms@gmail.com>
2019-01-13 12:17:41 -0600

    refactors save file, fixes no file error

--- saveFileExists.js
+++ saveFileExists.js
@@ -3,27 +3,75 @@ let fs = require('fs')
 let util = require('util')
 const repl = require('repl')
 const path = require('path');
-const electron = require('electron');
+// const electron = require('electron');
 
-const userDataPath = (electron.app || electron.remote.app).getPath('userData');
-var opts = {configName: 'user-data'};
+// const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+const userDataPath = 'userData';
+var opts = {
+  configName: 'user-data',
+  defaults: {}
+};
 
+// path where user data will be written to
 this.path = path.join(userDataPath, opts.configName + '.json');
+
+// returns the user data file, or if none exists, returns the defaults we specify
+function parseDataFile() {
+  try {
+    return JSON.parse(fs.readFileSync(this.path));
+  } catch(error) {
+    return opts.defaults;
+  }
+}
+
+function get(key) {
+  return this.data[key];
+}
+
+function set(key, val) {
+  this.data[key] = val;
+  fs.writeFileSync(this.path, JSON.stringify(this.data));
+}
+
+this.data = parseDataFile();
+console.log('User Data: ', this.data);
+
+function new_game(){
+  console.log('new_game click')
+  fs.writeFileSync('userData/user-data.json', opts.defaults);
+
+  // sets some random default data for new users
+  set('username', 'Steve Jones');
+  set('credits', 200)
+
+  $('#user-info').html(this.data.username);
+}
+
+function existing_game(){
+  // alert('load existing game clicked!')
+  //load save file and load next window
+}
+
+function existing_game(){
+  // alert('load existing game clicked!')
+  //load save file and load next window
+}
 
 var StartNewGame_button = $('<input type="button" value="Start New Game" id="new_game_button"/>')
 var ResumeGame_button = $('<input type="button" value="Resume Existing Game" id="existing_game_button"/>')
-var ResumeGame_button = $('<input type="button" value="Loading Screen" id="loading_screen_button"/>')
+var LoadingScreen_button = $('<input type="button" value="Loading Screen" id="loading_screen_button"/>')
 
-console.log('User Data: ', JSON.parse(fs.readFileSync(this.path)));
 
-if(fs.existsSync('gamesave.json')){
+// ********** RUNTIME CODE ************
+if(fs.existsSync(this.path)){
+  console.log('Found an existing game')
   //Return buttons for Start new game and load saved game
   $("#start_game_buttons").append(StartNewGame_button)
   $("#start_game_buttons").append(ResumeGame_button)
   // $("#start_game_buttons").append(LoadingScreen_button)
 
   $("#new_game_button").click(function(){
-    //Create new file, load game screen
+    //load existing save file and load next browser window
     new_game();
   });
 
@@ -38,49 +86,12 @@ if(fs.existsSync('gamesave.json')){
   });
 
 }else{
+  console.log('No existing game found')
   //Return button for Start new Game only
   $("#start_game_buttons").append(StartNewGame_button)
 
   $("#new_game_button").click(function(){
     //Create new file, load game screen (Page 2?)
     new_game()
-    })
-}
-
-// make a separate function for loading next window?
-function new_game(){
-  //alert('new game clicked!')
-  //create new save file and load next window
-  this.data = parseDataFile(this.path, opts.defaults);
-  set('username', 'Steve Jones');
-  $('#user-info').html(this.data.username);
-}
-
-function get(key) {
-  return this.data[key];
-}
-
-function set(key, val) {
-  this.data[key] = val;
-  fs.writeFileSync(this.path, JSON.stringify(this.data));
-}
-
-function parseDataFile(filePath, defaults) {
-  try {
-    return JSON.parse(fs.readFileSync(filePath));
-  } catch(error) {
-    return defaults;
-  }
-}
-
-function existing_game(){
-  // alert('load existing game clicked!')
-  //load save file and load next window
-
-}
-
-function existing_game(){
-  // alert('load existing game clicked!')
-  //load save file and load next window
-
+  });
 }
