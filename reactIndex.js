c3118a5078066ffd3b1837b3e058cea30cc6a53f
Steven Marks <steveprograms@gmail.com>
2019-01-13 18:49:35 -0600

    adds React and a sample component

---
+++ reactIndex.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Main = require('./react/main.js')
+
+ReactDOM.render(
+  React.createElement(Main),
+  document.getElementById('react-main')
+);
