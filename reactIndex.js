960a26df87efb22301aa4c38eb663ff73252e6e1
chanbot5000 <chandlersterling1@gmail.com>
2019-01-13 22:55:57 -0800

    Revert "Merge pull request #11 from steveprograms/adding_react"
    This reverts commit eb7bae65b9c84869084404e43a6d6c70718103a5, reversing
    changes made to 53252adbee74aeb0917189dd860a22e1d3d4cf43.

--- reactIndex.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Main = require('./react/main.js')
-
-ReactDOM.render(
-  React.createElement(Main),
-  document.getElementById('react-main')
-);
