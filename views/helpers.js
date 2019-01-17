fef4544ba62fabdb8dafcbee9c7f6156f169e4a8
Steven Marks <steveprograms@gmail.com>
2019-01-16 20:38:30 -0600

    comments for helpers

--- views/helpers.js
+++ views/helpers.js
@@ -1,5 +1,9 @@
 let fs = require('fs')
 
+// these are the same functions we use in the saveFileExists, but now we will
+// export them so we can save or load our json from any file
+// by doing `import {save, read} from '{path to }/helpers'`
+// they are then called in a file by simply writing `save(data)` and `read()`
 export function save(data) {
   fs.writeFileSync('./assets/data/user-data.json', JSON.stringify(data));
 }
