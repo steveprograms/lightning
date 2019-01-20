2b10f4169be4a92f79ca22cb1ba334113200d03d
Steven Marks <steveprograms@gmail.com>
2019-01-20 00:05:27 -0600

    replaces planet divs with mapPlanet components

--- views/mapPlanet.jsx
+++ views/mapPlanet.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import planets from '../assets/data/planets'
 
 export default class MapPlanet extends React.Component {
   render() {
-    { coordinates, color } = this.props;
+
+    let { name, xCoord, yCoord, color, diameter } = this.props;
+
+    xCoord = xCoord + 'px';
+    yCoord = yCoord + 'px';
+    diameter = (diameter || '15') + 'px';
+    name = name.toLowerCase();
+    let planet = planets[name];
+
     return (
       <div
-        style={{left: '150px', top: '302px', width: '5px', height: '5px', backgroundColor: 'yellow', position: 'absolute'}}/>
+        style={{left: xCoord, top: yCoord, width: '15px', height: '15px', backgroundColor: color, borderRadius: '50%', position: 'absolute'}}/>
     )
   }
 }
